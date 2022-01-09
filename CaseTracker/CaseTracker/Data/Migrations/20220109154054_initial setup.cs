3e4ee540951eecedcc389d6a66977a4a03cb8edb
Kevin Petow <kevin.m.petow@outlook.com>
2022-01-09 13:11:14 -0500

    connected db to display on Cases/Index. Also added link to navbar temporarily for easy access

---
+++ CaseTracker/CaseTracker/Data/Migrations/20220109154054_initial setup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaseTracker.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Case",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CaseStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VslName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VslOffNum = table.Column<int>(type: "int", nullable: false),
+                    ActivityId = table.Column<int>(type: "int", nullable: false),
+                    UserFirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserLastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactNum = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Case", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Case");
+        }
+    }
+}
