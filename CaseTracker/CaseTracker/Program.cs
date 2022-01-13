4956ab3669160cd4c6af221df7810e1abcf71eaa
Kevin Petow <kevin.m.petow@outlook.com>
2022-01-12 22:39:45 -0500

    updated json files for google auth

--- CaseTracker/CaseTracker/Program.cs
+++ CaseTracker/CaseTracker/Program.cs
@@ -1,6 +1,8 @@
 using CaseTracker.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
